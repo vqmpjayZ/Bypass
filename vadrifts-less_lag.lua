ff5205c56a41ab29a3492822b439f6db1f4bd042
vqmpjay <bakir.butkovic@osgrbavica2.edu.ba>
2024-11-26 18:48:10 +0100

    Update and rename vadrifts-less.lag to vadrifts-less_lag.lua

---
+++ vadrifts-less_lag.lua
@@ -0,0 +1,5 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Currently down";
+    Text = "Script is NOT up to date and is currently down. Join our discord for more: dsc.gg/vadriftz (in browsers)";
+    Duration = 10
+})
