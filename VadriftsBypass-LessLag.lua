4e6ed766d2c92a0187903e796f2ff638ad65c2e4
vqmpjay <bakir.butkovic@osgrbavica2.edu.ba>
2025-05-30 16:57:16 +0200

    Delete VadriftsBypass-LessLag.lua

--- VadriftsBypass-LessLag.lua
+++
@@ -1,7 +0,0 @@
---[[ Last Updated: 30.7.2024 (DD/MM/YY)
-Roblox Chat Bypasser Script (Less laggy version)
-
-join the discord: https//dsc.gg/vadriftz
---]]
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vqmpjayZ/Bypass/main/vadrifts-less.lag"))()
