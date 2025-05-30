ffdc7636b5e177ffba26e9fdd36624a7aeb3a51a
vqmpjay <bakir.butkovic@osgrbavica2.edu.ba>
2025-05-30 16:57:23 +0200

    Delete vadrifts-less_lag.lua

--- vadrifts-less_lag.lua
+++
@@ -1,5 +0,0 @@
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Currently down";
-    Text = "Script is NOT up to date and is currently down. Join our discord for more: dsc.gg/vadriftz (in browsers)";
-    Duration = 10
-})
